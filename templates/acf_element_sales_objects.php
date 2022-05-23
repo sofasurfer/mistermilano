ae3fa021ee3cbd021f0971066219e71859341856
Erin <erindazil@gmail.com>
2022-05-23 14:32:35 +0200

    Add sales content object and add support for the native ACF sorting (drag & drop)

--- templates/acf_element_sales_objects.php
+++ templates/acf_element_sales_objects.php
@@ -1,55 +1,59 @@
 <?php
-// ToDo: use an filter
+global $wp_query;
 $post_type = 'sales';
+$iteration = 0;
 
-if ( $site_element['selected'] == 1 ) {
-	$acf_posts = $site_element['items'];
-} else {
-	// Get posts
-	global $wp_query;
-	$news_query = array(
-		'post_type'      => $post_type,
-//		'orderby'        => $site_element['oderby'],
+$query = array(
+	'post_type'      => $post_type,
+	'posts_per_page' => 10,
+	'order'          => 'DESC',
+);
+
+if ( $site_element['selected'] ) {
+	$acf_posts = $site_element['selected'];
+
+    /** overwrites the query attributes with the ones needed for the "selected post list" **/
+	$query = array_merge( $query, [
+		'post__in'       => $acf_posts,
+		'orderby'        => 'post__in',
+		'posts_per_page' => - 1,
 		'order'          => 'ASC',
-		'posts_per_page' => - 1
-	);
-	$acf_posts  = get_posts( $news_query );
+	] );
 }
 
+$posts = get_posts( $query );
 ?>
 
 <!-- teaser small home -->
-<?php foreach ( $acf_posts as $post ): ?>
+<?php foreach ( $posts as $post ): ?>
 	<?php
+	$iteration ++;
+	$acf_image = get_post_thumbnail_id( $post );
+	$title     = get_field( 'title' );
 	// Get taxonomy name
 	$taxonomy = 'category';
 	if ( $post->post_type != 'post' ) {
 		$taxonomy = $post->post_type . '_category';
 	}
 	?>
-    <article class="c-container c-teaser-1col">
-        <div class="c-row">
-            <div class="c-col-7">
-                <figure class="c-teaser-img c-ratiobox c-ratiobox-16by9">
-                    <a href="<?= get_permalink( $post ); ?>">
-                    <?php $acf_image = get_post_thumbnail_id( $post ); ?>
-                    <?= do_shortcode( '[render_imagetag id="' . $acf_image . '" ]' ); ?>
-                    <?= wp_get_attachment_image($acf_image, 'full'); ?>
-                    </a>
-                </figure>
-            </div>
-            <div class="c-col-5 c-teaser-text  animation-element fade-up">
-                <span class="c-category-title"><?= get_the_date( 'd.m.Y', $post ); ?> / <?= do_shortcode( "[c_get_categories pid=\"$post->ID\" posttype=\"$taxonomy\"]" ); ?></span>
-                <a class="c-link-teaser" href="<?= get_permalink( $post ); ?>">
-                    <h3 class="c-teaser-title animation"><span><?= get_the_title( $post ); ?></span></h3>
-                </a>
-                <span class="c-category-title"><?= do_shortcode( "[c_get_categories pid=\"$post->ID\" posttype=\"$taxonomy\"]" ); ?></span>
-                <a class="c-link-teaser" href="<?= get_permalink( $post ); ?>">
-                    <h3 class="c-teaser-title animation"><span><?= get_field( 'acf_header_title', $post ); ?></span>
-                    </h3>
-                </a>
+    <div class="c-container-wide c-teaser-img-text c-line-top c-line-bottom">
+        <div class="c-container c-container-no-padding">
+            <!-- use c row reverse for switching img places-->
+            <div class="c-row <?php if ( $iteration % 2 == 0 ) { ?>c-row-reverse <?php } ?> ">
+                <div class="c-col-7 c-teaser-img-text-col-img">
+                    <figure>
+						<?= wp_get_attachment_image( $acf_image, 'full' ); ?>
+                    </figure>
+                </div>
+                <div class="c-col-5 c-teaser-img-text-col-text c-text-block c-text-padding-var">
+                    <span class="c-title-category"><?= __( 'Verkauf', 'neofluxe' ) ?> / <?= do_shortcode( "[c_get_categories pid=\"$post->ID\" posttype=\"$taxonomy\"]" ); ?></span>
+                    <h2><?= $title['bold'] ?> <span><?= $title['regular'] ?></span></h2>
+                    <p><?= $post->post_excerpt ?></p>
+                    <p><a class="c-icon c-link-arrow"
+                          href="<?= get_permalink( $post ); ?>"><?= __( 'mehr erfahren', 'neofluxe' ) ?></a></p>
+                </div>
             </div>
         </div>
-    </article>
+    </div>
 <?php endforeach; ?>
 
