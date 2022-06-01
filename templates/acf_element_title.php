4d1c40c8b7a5fdfe4719214f7442914f17ec12ee
Erin <erindazil@gmail.com>
2022-06-01 05:22:19 +0200

    Refactor Title HTML

--- templates/acf_element_title.php
+++ templates/acf_element_title.php
@@ -1,8 +1,10 @@
 <!-- section-title-->
-<div class="c-container c-container-no-padding c-title-section">
-    <div class="c-row">
-        <div class="c-col-10 c-text-block animation-element fade-up">
-            <h2 class="animation <?= ($site_element['small']) ? 'c-category-title' : ''; ?>"><?= $site_element['title']; ?></h2>
+<div class="c-container-wide c-title-secondary c-line-top">
+    <div class="c-container c-container-no-padding">
+        <div class="c-row">
+            <div class="c-col-9 c-text-padding">
+                <h2 class="c-h1 <?= ( $site_element['small'] ) ? 'c-category-title' : ''; ?>"><?= $site_element['title']; ?></h2>
+            </div>
         </div>
     </div>
 </div>
