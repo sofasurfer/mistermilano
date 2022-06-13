9bb96602cbf4b4333fb1717ec986a7b9fa82cd96
Erin <erindazil@gmail.com>
2022-06-13 13:15:24 +0200

    Refactor text element

--- templates/acf_element_text.php
+++ templates/acf_element_text.php
@@ -1,9 +1,7 @@
-<div class="c-container-wide c-title-secondary c-line-top">
-    <div class="c-container c-container-no-paddin c-text-element">
-        <div class="c-row">
-            <div class="c-col-9 c-text-padding">
-	            <?= do_shortcode("[render_animation_elements text='". $site_element['content'] ."']");?>
-            </div>
+<div class="c-container c-text-only">
+    <div class="c-row">
+        <div class="c-col-8 c-text-block c-text-padding">
+	        <?= $site_element['content'] ?>
         </div>
     </div>
 </div>
