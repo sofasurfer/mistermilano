6370847bfafde20fd989fffd9d23b091b045c853
Erin <erindazil@gmail.com>
2022-06-21 10:28:31 +0200

    Add ACF fields for icon selection, Refactor services sitelement to use icons

--- templates/acf_element_servicelisting.php
+++ templates/acf_element_servicelisting.php
@@ -1,7 +1,12 @@
 <!-- team teaser-->
 <?php
 global $wp_query;
-$post_type     = 'service';
+$post_type = 'service';
+$icons     = [
+	'rating'     => 'icon-rating',
+	'usage'      => 'icon-usage',
+	'renovation' => 'icon-renovation'
+];
 
 $query = array(
 	'post_type'   => $post_type,
@@ -15,13 +20,16 @@ $services = get_posts( $query );
 <div class="c-container-wide c-services c-line-top c-line-bottom">
     <div class="c-container c-container-no-padding">
         <div class="c-row">
-			<?php foreach ( $services as $post ) { ?>
+			<?php foreach ( $services as $post ) {
+                $fields = get_fields();
+				$selected_icon = $icons[ $fields['selected_icon'] ?? 'rating' ];
+				?>
                 <!-- Service -->
                 <div class="c-col-6 c-text-block">
-                    <h2 class="c-title-icon c-title-icon-rating"><?= $post->post_title ?></h2>
-	                <?php if ( $post->post_content ) { ?>
+                    <h2 class="c-title-icon c-title-<?= $selected_icon ?>"><?= $post->post_title ?></h2>
+					<?php if ( $post->post_content ) { ?>
                         <p><?= $post->post_content ?></p>
-	                <?php } ?>
+					<?php } ?>
                 </div>
                 <!-- END -->
 			<?php }; ?>
