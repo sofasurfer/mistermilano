1d6f15eccaa173fb31366b7e56aeef13eb28cb74
Erin <erindazil@gmail.com>
2022-06-28 13:34:20 +0200

    Refactor working static HTML variant with HMR (html + wp site both working)

---
+++ bud.config.mjs
@@ -0,0 +1,56 @@
+import createFontFile from './src/bud/createFontFile.mjs';
+
+/**
+ * @typedef {import('@roots/bud').Bud} bud
+ *
+ * @param {bud} bud
+ */
+
+export default async (bud) => {
+    createFontFile('./src/fonts/');
+
+    bud
+        .when(
+            bud.isProduction,
+            () => bud.hash().minimize(),
+        )
+
+        /**
+         * Application entrypoints
+         *
+         * Paths are relative to your resources directory
+         * meaning of the "@" symbol https://stackoverflow.com/a/42711271/7262739
+         */
+        .entry({
+            index: ['@src/scripts/index.js', '@src/styles/main.scss'],
+        })
+
+        /**
+         * These files should be processed as part of the build
+         * even if they are not explicitly imported in application assets.
+         */
+        .assets('images')
+
+        /**
+         * These files will trigger a full page reload
+         * when modified.
+         */
+        .watch('@src/**/*')
+
+        /**
+         * Target URL to be proxied by the dev server.
+         *
+         * This should be the URL you use to visit your local development server.
+         */
+        .proxy('http://localhost/')
+
+        /**
+         * Development URL to be used in the browser.
+         */
+        .serve('http://localhost:3010/nf-starter/')
+
+        /**
+         * Public path of application assets
+         */
+        .setPublicPath('/');
+};
