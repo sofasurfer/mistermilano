39bfcfe9bbb5f5ecf68e70ebf01be9be02b18041
Erin <erindazil@gmail.com>
2022-06-29 12:00:37 +0200

    Add missing files to release script

--- release.sh
+++ release.sh
@@ -10,10 +10,15 @@ perl -pe '/^Version: / and s/(\d+\.\d+\.\d+\.)(\d+)/$1 . ($2+1)/e' -i style.css
 #Sync files
 rsync -va ./acf-json/ ./_release/acf-json/
 rsync -va ./components/ ./_release/components/
+rsync -va ./src/ ./_release/src/
 rsync -va ./languages/ ./_release/languages/
 rsync -va ./dist/ ./_release/dist/
 rsync -va ./templates/ ./_release/templates/
 cp -v ./functions.php ./_release/
+cp -v ./bud.config.mjs ./_release/
+cp -v ./package.json ./_release/
+cp -v ./package-lock.json ./_release/
+cp -v ./yarn.lock ./_release/
 cp -v ./index.php ./_release/
 cp -v ./screenshot.png ./_release/
 cp -v ./style.css ./_release/
