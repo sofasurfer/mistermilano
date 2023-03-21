341882030168ce163ee731071d6c66af733f4419
Erin <erindazil@gmail.com>
2023-03-21 11:19:36 +0100

    Refactor: optimize readability of index

--- index.php
+++ index.php
@@ -1,21 +1,23 @@
 <?php
-get_template_part('templates/header');
+get_template_part( 'templates/header' );
 
 // Get Site Elements
 $page_id       = get_queried_object_id();
 $site_elements = get_field( 'acf_elements', $page_id );
+$post_type     = get_post_type( $page_id );
 
 if ( is_404() ) {
 	get_template_part( 'templates/page_404' );
 } else {
-	get_template_part( 'templates/page_' . get_post_type() );
+	get_template_part( 'templates/page_' . $post_type );
 
 	if ( ! empty( $site_elements ) ) {
 		foreach ( $site_elements as $site_element ) {
-			include( locate_template( 'templates/' . $site_element['acf_fc_layout'] . '.php', false, false ) );
+			$layout_name = $site_element['acf_fc_layout'];
+			include( locate_template( 'templates/' . $layout_name . '.php', false, false ) );
 		}
 	}
 }
 
-get_template_part('templates/footer');
+get_template_part( 'templates/footer' );
 ?>
